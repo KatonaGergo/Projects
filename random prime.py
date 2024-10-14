4546ea52b0f8f915d47f6ee8b976aac71fb19c15
Katona Gergő <159770907+KatonaGergo@users.noreply.github.com>
2024-10-14 17:55:53 +0200

    Add files via upload

--- random prime.py
+++ random prime.py
@@ -1,3 +1,4 @@
+'''
 def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -10,5 +11,31 @@ for i in range(2, 250001):
     if is_prime(i):
         primes.append(i)
 
+
+print(len(primes))
+'''
+
+import math
+
+
+def is_prime(n):
+    
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+primes = []
+
+
+for i in range(2, 10):
+    if is_prime(i):
+        primes.append(i)
 
 print(len(primes))
